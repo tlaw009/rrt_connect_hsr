91d7a5b1ddb24f17a5c4205f037269ede17065c6
shengxin <shengxin@shengxin-U-18.04-USB>
2020-11-16 14:13:57 -0500

    new

--- scripts/inverse_kinematics.py
+++ scripts/inverse_kinematics.py
@@ -5,7 +5,10 @@ from pydrake.multibody.parsing import Parser
 from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
-from pydrake.all import ConnectMeshcatVisualizer
+from pydrake.systems.meshcat_visualizer import ConnectMeshcatVisualizer
+from pydrake.geometry import FramePoseVector 
+from pydrake.math import RollPitchYaw
+from pydrake.manipulation.planner import DifferentialInverseKinematicsIntegrator, DifferentialInverseKinematicsParameters
 import os
 import numpy as np
 from pathlib import Path
@@ -20,7 +23,9 @@ from pathlib import Path
 # init builder and load multibody
 builder = DiagramBuilder()
 plant, _ = AddMultibodyPlantSceneGraph(builder, 1e-4)
-Parser(plant,_).AddModelFromFile(str(Path.home())+"/ros_drake_ws/src/RRT_connect_hsr/robots/hsrb_pub_release.urdf")
+hsr = Parser(plant,_).AddModelFromFile(str(Path.home())+"/ros_drake_ws/src/RRT_connect_hsr/robots/hsrb4s.obj.urdf")
+plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_footprint", hsr))
+
 plant.Finalize()
 
 # connect sim, build diagram and draw context
@@ -37,6 +42,13 @@ plant_context = plant.GetMyMutableContextFromRoot(context)
 
 plant.get_actuation_input_port().FixValue(plant_context, np.zeros(13))
 
+gripper = plant.GetBodyByName('hand_palm_link')
+
+pose = plant.EvalBodyPoseInWorld(plant_context, gripper)
+print(pose.translation())
+print(pose.rotation().matrix())
+
+IK = DifferentialInverseKinematicsIntegrator(plant, gripper.body_frame(), DifferentialInverseKinematicsParameters(1,1) , 0.01, plant_context, True)
 # advance sims
 simulator = Simulator(diagram, context)
 simulator.set_target_realtime_rate(0.1)
