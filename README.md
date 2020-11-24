aee64676a0d82e1c5e5dbedcdd004acafd9595be
tlaw009 <law9706@gmail.com>
2020-11-24 17:20:20 -0500

    solved IK, ROS serv pending

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RRT_connect_hsr
 RRT implementation for manipulation with HSR utilizing kinematic solver from drake
 
-Current: ```SetPositions()``` from ```DifferentialInverseKinematicsIntegrator``` asks for 52 joint positions instead of 26
+Current: Solved Inverse Kinematics, proceed to write a ROS service wrapper for it
 
 Note: repo name changed to RRT_connect_hsr
--- scripts/inverse_kinematics.py
+++ scripts/inverse_kinematics.py
@@ -11,6 +11,8 @@ from pydrake.math import RollPitchYaw , RotationMatrix, RigidTransform
 from pydrake.common.eigen_geometry import Isometry3
 from pydrake.manipulation.planner import DifferentialInverseKinematicsIntegrator, DifferentialInverseKinematicsParameters, DoDifferentialInverseKinematics
 from IPython.display import display, SVG
+from pydrake.multibody.inverse_kinematics import InverseKinematics
+from pydrake.solvers.mathematicalprogram import Solve
 import pydot
 import os
 import numpy as np
@@ -42,38 +44,44 @@ diagram.Publish(context)
 
 # draw multibody context
 plant_context = plant.GetMyMutableContextFromRoot(context)
-
+q0 = plant.GetPositions(plant_context)
 plant.get_actuation_input_port().FixValue(plant_context, np.zeros(13))
 
 gripper = plant.GetBodyByName('hand_palm_link')
-gripper_joint = plant.GetJointByName('hand_palm_joint')
-
-model_index = ModelInstanceIndex(2)
-print(model_index.is_valid())
-print(plant.GetModelInstanceName(model_index))
-# hsrb_model = plant.GetModelInstanceByName(plant.GetModelInstanceName(model_index))
-hsr_joint_indices = plant.GetJointIndices(model_index)
-# print(hsr_joint_indices)
-
-print(plant.get_joint(hsr_joint_indices[26]))
 
 pose = plant.EvalBodyPoseInWorld(plant_context, gripper)
+
 print(pose.translation())
 print(pose.rotation().matrix())
-pt = [pose.translation()[0]+0.01,pose.translation()[1]+0.01,pose.translation()[2]+0.01]
+pt = [pose.translation()[0]+0.2,pose.translation()[1],pose.translation()[2]]
 
 gripper_end_effect_position = pt
 gripper_end_effect_rotation = pose.rotation()
-gripper_end_effect_pose = Isometry3(gripper_end_effect_rotation.matrix(),gripper_end_effect_position)
 
-IK = DifferentialInverseKinematicsIntegrator(plant, gripper.body_frame(), 0.1, DifferentialInverseKinematicsParameters(len(plant.GetPositions(plant_context)),len(plant.GetVelocities(plant_context))), plant_context, True)
-print(plant.GetPositions(plant_context))
-# IK.SetPositions(plant_context,plant.GetPositions(plant_context))
-pose_1 = IK.ForwardKinematics(plant_context)
-joint_v = DoDifferentialInverseKinematics(plant, plant_context,gripper_end_effect_pose, gripper.body_frame(),DifferentialInverseKinematicsParameters(len(plant.GetPositions(plant_context)),len(plant.GetVelocities(plant_context))))
-print(joint_v.status)
-print(joint_v.joint_velocities)
-# # # advance sims
+ik = InverseKinematics(plant,plant_context)
+ik.AddPositionConstraint(gripper.body_frame(), [0,0,0], plant.world_frame(), gripper_end_effect_position, gripper_end_effect_position)
+ik.AddOrientationConstraint(gripper.body_frame(), RotationMatrix(), plant.world_frame(), gripper_end_effect_rotation, 0.0)
+
+prog = ik.get_mutable_prog()
+q = ik.q()
+
+prog.AddQuadraticErrorCost(np.identity(len(q)), q0, q)
+prog.SetInitialGuess(q, q0)
+
+result = Solve(ik.prog())
+
+if not result.is_success():
+	print("IK failed")
+
+print(result.GetSolution())
+# IK = DifferentialInverseKinematicsIntegrator(plant, gripper.body_frame(), 0.1, DifferentialInverseKinematicsParameters(len(plant.GetPositions(plant_context)),len(plant.GetVelocities(plant_context))), plant_context, True)
+# print(plant.GetPositions(plant_context))
+# # IK.SetPositions(plant_context,plant.GetPositions(plant_context))
+# pose_1 = IK.ForwardKinematics(plant_context)
+# joint_v = DoDifferentialInverseKinematics(plant, plant_context,gripper_end_effect_pose, gripper.body_frame(),DifferentialInverseKinematicsParameters(len(plant.GetPositions(plant_context)),len(plant.GetVelocities(plant_context))))
+# print(joint_v.status)
+# print(joint_v.joint_velocities)
+# # # # advance sims
 
 # simulator = Simulator(diagram, context)
 # simulator.set_target_realtime_rate(1)
